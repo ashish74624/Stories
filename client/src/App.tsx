f33b237be3488847567a6de94c1a0c6be2cb44e8
Ashish <ashish74624@gmail.com>
2023-12-24 20:00:11 +0530

    Landing Page ready

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,13 @@
-
+import { BrowserRouter as Router, Routes , Route } from "react-router-dom"
+import Landing from "./pages/Landing"
 function App() {
 
   return (
-    <>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing/>}/>
+      </Routes>
+    </Router>
   )
 }
 
